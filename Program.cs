8015630ebedbd3ad1a14b9a23d9c08592a2b2e17
Matthew Rizzini <matthewrizzini@gmail.com>
2018-05-21 19:24:28 -0400

    pulling data from stock list and logging to console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
@@ -31,12 +32,47 @@ namespace FinanceTest
             userPasswordField.SendKeys("scrapePass");
             loginPasswordButton.Click();
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5); 
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15); 
 
 
-            var portfolioButton = driver.FindElement(By.XPath("//*[@id='Nav-0-DesktopNav']/div/div[3]/div/div[1]/ul/li[2]/a"));
+            var portfolioButton = driver.FindElement(By.XPath("//*[@id='Nav-0-DesktopNav']/div/div[3]/div/div[1]/ul/li[2]"));
 
             portfolioButton.Click();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);  
+
+            var popups = driver.FindElements(By.XPath("//*[@id='fin-tradeit']/div[2]/div/div/div[2]/button[2]"));
+
+            // var popupButton = driver.FindElement(By.XPath("//*[@id='fin-tradeit']/div[2]/div/div/div[2]/button[2]"));
+
+            if (popups.Count > 0)
+            {
+                System.Console.WriteLine("popup is {0}", popups[0]);
+                popups[0].Click();
+            }
+            else
+            {
+                System.Console.WriteLine("Popup not found");
+            }
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);  
+            
+            var myScraperButton = driver.FindElement(By.XPath("//*[@id='main']/section/section/div[2]/table/tbody/tr[2]/td[1]/a"));
+
+            myScraperButton.Click();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);  
+            
+            var stockList = driver.FindElements(By.XPath("//*[@id='main']/section/section[2]/div[2]/table/tbody/tr"));
+
+            System.Console.WriteLine("stocklist count is {0}", stockList.Count);
+
+            foreach (var stock in stockList)
+            {
+                System.Console.WriteLine("Stock is " + stock.FindElement(By.ClassName("_1_2Qy")).Text);
+            }
+
+
 
             // Actions action = new Actions(driver);
             // IWebElement portfolioButton = driver.FindElement(By.XPath("//*[@id='Nav-0-DesktopNav']/div/div[3]/div/div[1]/ul/li[2]/a"));
@@ -80,6 +116,22 @@ namespace FinanceTest
  
             // action.Perform();
 
+
+
+
+            // var elemTable = driver.FindElement(By.XPath("//div[@id='mw-content-text']//table[1]"));
+
+            // var lstTdElem = new List<IWebElement>(elemTr.FindElements(By.TagName("td")));
+            // if (lstTdElem.Count > 0)
+            // {
+            //     foreach (var elemTd in lstTdElem)
+            //     {
+            //         // "\t\t" for Tab Space
+            //         strRowData = strRowData + elemTd.Text + "\t\t";
+            //     }
+            // }
+            // Console.WriteLine(strRowData);
+            // strRowData="";
 
             
 
